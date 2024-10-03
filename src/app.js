08c74a67be6211a102b8d3a0c17f4b01c574e5e6
AdityaMali918 <adityamali918@gmail.com>
2024-10-03 10:42:49 +0530

    login logout auth jwttoken

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(express.json({limit:"16kb"}));
 app.use(express.urlencoded({extended:true,limit:'16kb'}))
 //Public Folder
 app.use(express.static("public"))
-//cookie
+//cookie (use to access cookie)
 app.use(cookieParser())
 
 
